e010df89d634a8f1f8d6ed50366421d3af0bbf06
Ismoil <ismoillmirabbosoff@gmail.com>
2024-07-15 22:02:11 +0500

    feat: feat new configurations

--- agro/settings.py
+++ agro/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'postgres', 
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'agrodiamond', 
+        'HOST': 'agrodb', 
         'PORT': '5433',
     }
 }
